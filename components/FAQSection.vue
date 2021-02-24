5b2c181052e27e678ba54a813ecafbdb98eef031
apoorvdwi <apoorvd14@gmail.com>
2021-02-25 01:42:27 +0530

    added riddle section and date in faq fixed

--- components/FAQSection.vue
+++ components/FAQSection.vue
@@ -49,7 +49,7 @@ export default {
         {
           title: "How long will it be on for?",
           content:
-            "The event will be running for about one week from 10th April to 18th April."
+            "The event will be running for about one week from 12th June to 20th June."
         },
         {
           title: "Who can participate?",
