d411cdc44785e32adca3b841490d27ccf949bdd5
apoorvdwi <apoorvd14@gmail.com>
2021-04-08 03:50:11 +0530

    added mlh coc

--- components/Footer.vue
+++ components/Footer.vue
@@ -17,7 +17,8 @@
 
         <div class="panel">
           <h4 class="title">About HackOn Foundation</h4>
-          <a href="/coc">Code Of Conduct</a>
+          <a href="/coc">Event Code Of Conduct</a>
+          <a href="/mlhcoc">MLH Code Of Conduct</a>
           <a href="/team">Team Behind HackOn</a>
         </div>
 
