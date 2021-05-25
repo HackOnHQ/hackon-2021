0aba2712b4007fd18b3c4327c198e18b3d5eea5d
Paras Gupta <parasg1999@gmail.com>
2021-05-26 01:18:40 +0530

    refactor: speakers and judges

---
+++ pages/judgesAndMentors.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="judges-and-mentors-page">
+    <SpeakersSection />
+  </div>
+</template>
+
+<script>
+import SpeakersSection from "~/components/JudgesAndMentorsSection";
+
+export default {
+  components: {
+    SpeakersSection,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.team-page {
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+}
+</style>
